2309f603a27ef012672f3b0e02d63d6c5b480fc4
Eliabe <eliabepaz.wprk@gmail.com>
2020-06-02 15:35:10 -0300

    .

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php 
-echo'here we go again!';
+error_reporting(0);
+session_start();
+$imagem = $_GET['imagem'];
+?>
+<html>
+<head>
+        <title>Upload de imagens com PHP</title>
+        <meta charset="utf-8"/>
+</head>
+<body>
+    <form action="index.php" method="get" enctype="multipart/form-data">
+        <label for="imagem">Imagem:</label>
+        <input type="file" name="imagem"/>
+        <br/>
+        <input type="submit" value="Enviar"/>
+    </form>
+</body>
+</html>
+<?php
+if ($imagem){
+    $_SESSION['imagem'] = $imagem;
+    header('Location: gravar.php');
+}
