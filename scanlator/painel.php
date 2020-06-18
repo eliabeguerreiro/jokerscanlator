eada3eea1b0000bab36aa2590d2642da8b9c8119
Eliabe <eliabepaz.wprk@gmail.com>
2020-06-18 15:40:10 -0300

    definido paines e seus respectivos acessos

--- scanlator/painel.php
+++ scanlator/painel.php
@@ -1,32 +1,117 @@
 <?php 
-error_reporting(0);
 session_start();
-include_once('conexao.php');
-
+include_once('../funcoes/conexao.php');
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="pt">
 <head>
-        <title>Painel de Projetos</title>
-        <meta charset="utf-8"/>
-</head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/js/all.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+  </head>
 <body>
-    
-    <label for="projeto">Projetos:</label></br>
-    <?php
 
-//aqui vão aparecer todos os projetos existentes
+  <div class = "wrapper">
+
+    <nav id="sidebar">
+
+      <div class="sidebar-header col">
+        <h3>Painel Joker</h3>
+        <button type="button " id="sidebarCollapse" class="btn btn-danger col " href="#">
+          Sair
+        </button><br>
+      </div>
+      <ul class="list-unstyled components">
+
+        <p>Atividades</p>
+        <li>
+          <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Cadastrar</a>
+          <ul class="collapse list-unstyled" id="homeSubmenu">
+            <li>
+              <a href="#">Novo Capítulo</a>
+            </li>
+            <li>
+              <a href="#">Novo Projeto</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Postar</a>
+          <ul class="collapse list-unstyled" id="pageSubmenu">
+            <li>
+              <a href="#">Postar Lançamento</a>
+            </li>
+            <li>
+              <a href="#">Postar Aviso</a>
+            </li>
+          </ul>
+        </li>
+        
+        <li>
+          <a href="#pageSubedit" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Editar</a>
+          <ul class="collapse list-unstyled" id="pageSubedit">
+            <li>
+              <a href="#">Editar Capítulo</a>
+            </li>
+            <li>
+              <a href="#">Postar Aviso</a>
+            </li>
+          </ul>
+        </li>
+        
+        <li>
+          <a href="#desesTressar" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Desestressar</a>
+          <ul class="collapse list-unstyled" id="desesTressar">
+            <li>
+              <a href="#">Xingar o Matsu</a>
+            </li>
+            <li>
+              <a href="#">Ver Foto de Deus</a>
+            </li>
+          </ul>
+        </li>
+        <li>          
+          
+        </li>
+        
+      </ul>
+    </nav>
+
+    <div id="content">
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-ligth">
+        
+        
+        
+        
+        
+        
+        <div class="position-relative float-left">
+          
+        </div>
+
+
+      </nav>
+
+    </div>
 
 
 
-echo "<a href='projetos/peerless_dad/peerless_dad.php'>Peerless Dad</a></br>";
-echo "<a href='projetos/yuru_camp/yuru_camp.php'>Yuru Camp</a></br>";
+
+
+  </div>
+  
+  
 
 
 
 
 
 
-//acesso ao sistema de postagem do blog
 
 
 
@@ -34,9 +119,24 @@ echo "<a href='projetos/yuru_camp/yuru_camp.php'>Yuru Camp</a></br>";
 
 
 
-echo "</br></br><a href='../../index.php'>Voltar</a>";
-?>
-       
-</body>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+    <script>
+      
+      $(document).ready(function(){
+        $("#sidebarCollapse").on('click', function(){
+          $("#sidebar").toggleClass('active')
+        });
+      });
+
+      
+
+
+
+    </script>
+  </body>
 </html>
-<?php
